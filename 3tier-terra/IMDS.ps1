b31379de2209d712f599c82ce49b236620ba6a05
thirumaldage <87520379+thirumaldage@users.noreply.github.com>
2022-11-15 17:41:31 +0530

    Update IMDS.ps1

--- 3tier-terra/IMDS.ps1
+++ 3tier-terra/IMDS.ps1
@@ -1,18 +1,18 @@
 # #On the VM to read
 # #Raw data
-# $respraw=Invoke-WebRequest -Headers @{"Metadata"="true"} -Method GET -Proxy $Null -Uri "http://169.254.169.254/metadata/instance?api-version=2021-01-01"
-# $respraw
-# $respraw.Content
-# $respraw.Content | ConvertFrom-Json | ConvertTo-Json -Depth 6
+ $respraw=Invoke-WebRequest -Headers @{"Metadata"="true"} -Method GET -Proxy $Null -Uri "http://169.254.169.254/metadata/instance?api-version=2021-01-01"
+ $respraw
+ $respraw.Content
+ $respraw.Content | ConvertFrom-Json | ConvertTo-Json -Depth 6
 
 # #Instance details
-# $resp=Invoke-RestMethod -Headers @{"Metadata"="true"} -Method GET -Proxy $Null -Uri "http://169.254.169.254/metadata/instance?api-version=2021-01-01"
-# $respJSON = $resp | ConvertTo-Json -Depth 6
+ $resp=Invoke-RestMethod -Headers @{"Metadata"="true"} -Method GET -Proxy $Null -Uri "http://169.254.169.254/metadata/instance?api-version=2021-01-01"
+ $respJSON = $resp | ConvertTo-Json -Depth 6
 
 # #Compute only, could do Network, all the main JSON headings
-# Invoke-RestMethod -Headers @{"Metadata"="true"} -Method GET -Proxy $Null -Uri "http://169.254.169.254/metadata/instance/compute?api-version=2021-01-01"
+ Invoke-RestMethod -Headers @{"Metadata"="true"} -Method GET -Proxy $Null -Uri "http://169.254.169.254/metadata/instance/compute?api-version=2021-01-01"
 
-# #Just get tags
+ #Just get tags
 # Invoke-RestMethod -Headers @{"Metadata"="true"} -Method GET -Proxy $Null -Uri "http://169.254.169.254/metadata/instance/compute/tagsList?api-version=2021-01-01"
 
 # #size
